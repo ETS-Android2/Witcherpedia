99bcb871a3e70480344ed89ca9dab6386968430c
StTsk <stavroula4ever@gmail.com>
2020-10-29 19:41:27 +0200

    Update db with Ofier unit icons

--- WitcherpediaFolder/app/src/main/java/com/example/drhappy/witcherpedia/DBHelper.java
+++ WitcherpediaFolder/app/src/main/java/com/example/drhappy/witcherpedia/DBHelper.java
@@ -402,11 +402,11 @@ class DBHelper extends SQLiteOpenHelper {
 
 		// missing a lot
 
-		db.execSQL("INSERT INTO Unit VALUES('Immortal', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 1, 'ic_default_unit') ");
-		db.execSQL("INSERT INTO Unit VALUES('Immortal Camelier', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 2, 'ic_default_unit') ");
-		db.execSQL("INSERT INTO Unit VALUES('Ofieri Charioteer', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 3, 'ic_default_unit') ");
-		db.execSQL("INSERT INTO Unit VALUES('Ofieri Runewright', 'Ofier', 'Support', 'Silver', 'Ofieri', 4, 'ic_default_unit') ");
-		db.execSQL("INSERT INTO Unit VALUES('Ofieri Sorcerer', 'Ofier', 'Mage', 'Silver', 'Ofieri', 5, 'ic_default_unit') ");
+		db.execSQL("INSERT INTO Unit VALUES('Immortal', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 1, 'immortal') ");
+		db.execSQL("INSERT INTO Unit VALUES('Immortal Camelier', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 2, 'immortal_camelier') ");
+		db.execSQL("INSERT INTO Unit VALUES('Ofieri Charioteer', 'Ofier', 'Melee', 'Bronze', 'Ofieri', 3, 'ofieri_charioteer') ");
+		db.execSQL("INSERT INTO Unit VALUES('Ofieri Runewright', 'Ofier', 'Support', 'Silver', 'Ofieri', 4, 'ofieri_runewright') ");
+		db.execSQL("INSERT INTO Unit VALUES('Ofieri Sorcerer', 'Ofier', 'Mage', 'Silver', 'Ofieri', 5, 'ofieri_sorcerer') ");
 
 		db.execSQL("INSERT INTO Unit VALUES('Zerrikanian Swordsman', 'Zerrikania', 'Melee', 'Bronze', 'Zerrikanian', 1, 'zerrikanian_swordsman') ");
 		db.execSQL("INSERT INTO Unit VALUES('Zerrikanian Archer', 'Zerrikania', 'Ranged', 'Bronze', 'Zerrikanian', 2, 'zerrikanian_archer') ");
